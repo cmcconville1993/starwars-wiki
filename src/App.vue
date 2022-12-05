6891167a5119f6014a42d79be18378f0a70b88e3
Ciaran McConville <cmcconville@dxc.com>
2022-12-05 00:15:09 +0000

    Character Details

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CharacterDetails msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CharacterDetails from './components/CharacterDetails.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CharacterDetails
   }
 }
 </script>
---
+++ src/components/CharacterDetails.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <img v-bind:src="info.img" />
+        <div>{{ info.name }}</div>
+    </div>
+</template>
+  
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            info: null
+        }
+    },
+    mounted() {
+        axios
+            .get('https://breakingbadapi.com/api/characters/1')
+            .then(response => {
+                console.log('yupppaaaa')
+                this.info = response.data[0]
+                console.log(JSON.stringify(this.details))
+            })
+    }
+
+
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
+  
