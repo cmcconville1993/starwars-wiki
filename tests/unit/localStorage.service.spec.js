7b3ca300cd2cecb589336375375a4b0a11ebfc20
Ciaran McConville <cmcconville@dxc.com>
2022-12-14 22:41:59 +0000

    init

---
+++ tests/unit/localStorage.service.spec.js
@@ -0,0 +1,8 @@
+
+test('Write to client-side storage', () => {
+    throw new Error('Not Implemented')
+})
+
+test('read from client-side storage', () => {
+    throw new Error('Not Implemented')
+})
