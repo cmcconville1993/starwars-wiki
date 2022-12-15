8a762dcbce6ee3d20dda976de76003e7c9398a62
Ciaran McConville <cmcconville@dxc.com>
2022-12-15 20:54:40 +0000

    Local storage service

--- src/services/localStorage.service.js
+++ src/services/localStorage.service.js
@@ -6,3 +6,12 @@ export function Write(key, value) {
 export function Read(key) {
     return localStorage.getItem(key)
 }
+
+export function Remove(key) {
+    return localStorage.removeItem(key)
+}
+
+export function GetAllFromLocalStorage() {
+    const likedCharacters = { ...localStorage };
+    return likedCharacters
+}
--- tests/unit/localStorage.service.spec.js
+++ tests/unit/localStorage.service.spec.js
@@ -1,4 +1,4 @@
-import { Write, Read } from '@/services/localStorage.service'
+import { Write, Read, Remove, GetAllFromLocalStorage } from '@/services/localStorage.service'
 
 test('Write to client-side storage', () => {
     //Arrange
@@ -23,3 +23,34 @@ test('read from client-side storage', () => {
     //Assert
     expect(result).toBe('value-1')
 })
+
+test('remove from client-side storage', () => {
+    //Arrange
+    localStorage.setItem('key-1', 'value-1')
+
+    //Act
+    Remove('key-1')
+
+
+    //Assert
+    const result = localStorage.getItem('key-1')
+    expect(result).toBeNull()
+})
+
+test('Get all from client-side storage', () => {
+    //Arrange
+    localStorage.setItem('key-1', 'value-1')
+    localStorage.setItem('key-2', 'value-2')
+    localStorage.setItem('key-3', 'value-3')
+    localStorage.setItem('key-4', 'value-4')
+
+    //Act
+    const allLiked = GetAllFromLocalStorage()
+
+    //Assert
+    expect(allLiked).toStrictEqual({ "key-1": "value-1", "key-2": "value-2", "key-3": "value-3", "key-4": "value-4" })
+})
+
+beforeEach(() => {
+    window.localStorage.clear();
+});
